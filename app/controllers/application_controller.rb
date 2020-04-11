c59b6d41114383767da4977462d4db0efc4a1f4d
kawahara <hideaki-kawahara@bright-system.jp>
2020-04-11 23:14:14 +0900

    Revert "Add devise omniauth"

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
-  protected
-  #登録時と編集時に独自カラムのストロングパラメータ追加
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: %i(name uid image provider))
-      devise_parameter_sanitizer.permit(:account_update, keys: %i(name uid image provider))
-    end
 end
