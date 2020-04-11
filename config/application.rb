fc079e797ec3a5648f69541a959085430c2acf5f
Hideaki Kawahara <hideaki-kawahara@bright-system.jp>
2020-04-12 00:26:09 +0900

    retry update Precompiling assets failed.

--- config/application.rb
+++ config/application.rb
@@ -15,8 +15,8 @@ module TwitterLogin
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.i18n.default_locale = :ja #日本語化する
-    config.time_zone = 'Asia/Tokyo' #タイムゾーンを日本時間にする
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Asia/Tokyo'
     config.assets.initialize_on_precompile = false
     config.assets.js_compressor = Uglifier.new(harmony: true)
   end
